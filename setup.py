e4fc34f08f427feb152c399b05d9874eb38cc0e3
Thomas Preston <thomas.mark.preston@gmail.com>
2013-02-15 13:32:10 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='spi',
 	description='Python module for communicating with an SPI device.',
 	author='Thomas Preston',
 	author_email='thomasmarkpreston@gmail.com',
-	license='GPLv3',
+	license='GPLv2',
 	url='http://pi.cs.man.ac.uk/interface.htm',
 	ext_modules=[Extension('spi', ['spi.c'])],
 )
